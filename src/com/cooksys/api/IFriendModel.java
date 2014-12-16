7e6a96c79ce50c9db3f16444a8de8576ff60653f
mookd <mook_272@hotmail.com>
2014-12-16 12:37:24 -0600

    add get/set methods

--- src/com/cooksys/api/IFriendModel.java
+++ src/com/cooksys/api/IFriendModel.java
@@ -4,19 +4,22 @@ import java.util.Date;
 
 public interface IFriendModel {
 
-	public Date getDateCreated();
-	
 	/**
 	 * Requires: 0<=status<=2
 	 * @param status
 	 */
 	public void setStatus(int status);
 	
-	public void getStatus();
+	public Integer getStatus();
 	
 	public IAccountModel getFrienderAccountModel();
 	
-	public IAccountModel getFriendeeAccountMode();
+	public IAccountModel getFriendeeAccountModel();
+
+	public Date getCreatedDate();
+
+	public Date getUpdatedDate();
+
 	
 	
 	
